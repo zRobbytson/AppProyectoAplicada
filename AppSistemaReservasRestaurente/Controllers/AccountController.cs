0736ad6dcb1d1f0564f22bdd877e0df7c36ac748
anderyncil <dyncilr22_2@unc.edu.pe>
2025-08-24 01:14:41 -0500

    botones de iniciar sesio y cerar sesion corregidos

---
+++ AppSistemaReservasRestaurente/Controllers/AccountController.cs
@@ -0,0 +1,25 @@
+﻿using AppSistemaReservasRestaurente.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AppSistemaReservasRestaurente.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly SignInManager<ApplicationUser> _signInManager;
+
+        public AccountController(SignInManager<ApplicationUser> signInManager)
+        {
+            _signInManager = signInManager;
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Logout()
+        {
+            await _signInManager.SignOutAsync();
+            return RedirectToAction("Index", "Home"); // 👈 Redirige al Home después de cerrar sesión
+        }
+    }
+}
